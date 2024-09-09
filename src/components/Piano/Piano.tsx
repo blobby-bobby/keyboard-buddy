2304d9ec11a91c43abfea51a25db60f22641d6a8
blobby-bobby <bulle.ouvrard@gmail.com>
2024-09-09 20:11:44 +0200

    feat(wip): piano styles

---
+++ src/components/Piano/Piano.tsx
@@ -0,0 +1,96 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+import { keyboardKeys, pianoKeysMap } from "../../utils/piano/pianoKeysMap";
+import { playNote } from "../../utils/piano/playNote";
+import { initializeAudioContext } from "../../utils/context/initializeAudioContext";
+import { logSilence } from "../../utils/logSilence";
+import "./style.css";
+import { IsPlayingContext } from "../../utils/context/isPlayingContext";
+
+const Piano = () => {
+  const [activeKeys, setActiveKeys] = useState<Set<string>>(new Set());
+  const { setIsPlaying } = useContext(IsPlayingContext);
+
+  const handlePlayNote = useCallback(async (key: string) => {
+    await initializeAudioContext();
+    if (!keyboardKeys.includes(key)) {
+      logSilence();
+      return;
+    }
+    playNote(key);
+    setActiveKeys((prev) => new Set(prev).add(key));
+  }, []);
+
+  const handleKeyDown = useCallback(
+    async (event: KeyboardEvent) => {
+      const key = event.key.toLowerCase();
+      if (!activeKeys.has(key)) {
+        await handlePlayNote(key);
+      }
+    },
+    [activeKeys]
+  );
+
+  const handleKeyUp = useCallback(
+    (event: KeyboardEvent) => {
+      const key = event.key.toLowerCase();
+      inactivateKeys(key);
+    },
+    [activeKeys]
+  );
+
+  const inactivateKeys = useCallback(
+    (key: string) => {
+      setActiveKeys((prev) => {
+        const newSet = new Set(prev);
+        newSet.delete(key);
+        return newSet;
+      });
+    },
+    [activeKeys]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [activeKeys]);
+
+  useEffect(() => {
+    let timeOut: ReturnType<typeof setTimeout>;
+
+    if (!activeKeys.size) {
+      timeOut = setTimeout(() => {
+        setIsPlaying(false);
+      }, 750);
+    } else {
+      setIsPlaying(true);
+    }
+
+    return () => clearTimeout(timeOut);
+  }, [activeKeys.size]);
+
+  return (
+    <>
+      <div className="piano">
+        {Object.entries(pianoKeysMap).map(([key, value]: [string, string]) => (
+          <div key={key} className="piano-key-wrapper">
+            <span>{value}</span>
+            <button
+              className={`piano-key ${activeKeys.has(key) ? "active" : ""}`}
+              onMouseDown={() => handlePlayNote(key)}
+              onMouseUp={() => inactivateKeys(key)}
+            >
+              {key}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Piano;
