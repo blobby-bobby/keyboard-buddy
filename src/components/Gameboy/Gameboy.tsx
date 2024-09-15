0353e5155bd0dd2c5fa8befcfb188fddaaeaf4f6
blobby-bobby <bulle.ouvrard@gmail.com>
2024-09-15 18:28:56 +0200

    feat: add reset button, to be activated soon

--- src/components/Gameboy/Gameboy.tsx
+++ src/components/Gameboy/Gameboy.tsx
@@ -1,6 +1,6 @@
 import Piano from "../Piano/Piano";
 import Screen from "../Screen/Screen";
-import "./style.css";
+import "./styles.css";
 
 const Gameboy = () => {
   return (
@@ -12,10 +12,17 @@ const Gameboy = () => {
         <Screen />
         <Piano />
 
-        <div className="speakers">
-          {Array.from({ length: 6 }, (_, index) => (
-            <div key={index} className="speaker-line"></div>
-          ))}
+        <div className="gameboy-bottom">
+          <div className="button-down">
+            <p>Reset</p>
+            <button className="reset" type="button"></button>
+          </div>
+
+          <div className="speakers">
+            {Array.from({ length: 6 }, (_, index) => (
+              <div key={index} className="speaker-line"></div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
