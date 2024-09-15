3505d7f553f14b7429fb0b107b22e1fe43fd9627
blobby-bobby <bulle.ouvrard@gmail.com>
2024-09-15 17:54:33 +0200

    feat: improved instructions styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { IsPlayingProvider } from "./utils/context/playWithBuddyContext";
 function App() {
   return (
     <IsPlayingProvider>
-      <p>Play with your keyboard</p>
+      <p className="instructions">
+        Press <kbd>q</kbd>, <kbd>s</kbd>, <kbd>d</kbd>, or <kbd>whatever</kbd>{" "}
+        on the line of your keyboard to play music.
+      </p>
       <Gameboy />
     </IsPlayingProvider>
   );
