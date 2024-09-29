eac34d144dd88ae5b745c85dced93ea8744f6785
blobby-bobby <bulle.ouvrard@gmail.com>
2024-09-29 19:19:00 +0200

    feat(wip): temp melodies to cure buddy feelings data

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import { KeyboardKey } from "./piano/pianoKeysMap";
+
 // BUDDY FEELINGS
 const eventsFeelings = {
   hungry: "hungry",
@@ -5,10 +7,19 @@ const eventsFeelings = {
   idle: "idle",
 } as const;
 
-type EnumValue<T> = T[keyof T];
+export type EnumValue<T> = T[keyof T];
 export type BuddyFeeling = EnumValue<typeof eventsFeelings>;
 
 // TIMER INTERVALS
-export const MIN_INTERVAL = 1 * 60 * 1000; // 1 minute
-export const MAX_INTERVAL = 2 * 60 * 1000; // 2 minutes
-export const LOWER_HAPPINESS_INTERVAL = 10000; // Decrease happiness every 10 seconds
+export const MIN_INTERVAL = 0.1 * 60 * 1000; // 1 minute
+export const MAX_INTERVAL = 0.2 * 60 * 1000; // 2 minutes
+export const LOWER_HAPPINESS_INTERVAL = 5000; // Decrease happiness every 20 seconds
+
+// MELODIES TO HANDLE BUDDY FEELINGS
+export const buddyMelodies: Record<
+  Exclude<BuddyFeeling, "idle">,
+  KeyboardKey[]
+> = {
+  hungry: ["q", "z", "d"],
+  dirty: ["g", "h", "j"],
+} as const;
